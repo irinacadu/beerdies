ab9363d93ed62fb34c5c0e347b709dd37fc26d08
Irina Casas <67893270+irinacadu@users.noreply.github.com>
2022-01-26 09:28:15 +0100

    subida beerdies

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template class="general">
+  <div>
+    <Cab />
+    <i class="fas fa-shopping-basket"></i>
+  </div>
+  <div>
+    <Bread />
+  </div>
+  <div>
+    <Imagenes />
+  </div>
+  <div>
+    <Descripcion />
+  </div>
+  <div class="central">
+    <div>
+      <Video />
+    </div>
+    <div>
+      <birras />
+    </div>
+  </div>
+
+  <div>
+    <Pie />
+  </div>
+</template>
+
+<script>
+import Cab from "./components/Cab.vue";
+import Bread from "./components/Bread.vue";
+import Imagenes from "./components/Imagenes.vue";
+import Descripcion from "./components/Descripcion.vue";
+import Video from "./components/Video.vue";
+import birras from "./components/birras.vue";
+import Pie from "./components/Pie.vue";
+
+export default {
+  name: "App",
+  components: {
+    Cab,
+    Bread,
+    Imagenes,
+    Descripcion,
+    Video,
+    birras,
+    Pie,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&family=Monoton&family=Petit+Formal+Script&family=Urbanist:wght@300&display=swap");
+#nEmpre {
+  text-align: center;
+}
+
+body {
+  width: 2000px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fffcfa;
+}
+
+.central {
+  display: flex;
+  gap: 6rem;
+}
+.general {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Pie.vue
@@ -0,0 +1,161 @@
+<template>
+  <footer class="pie">
+    <div class="enlaces">
+      <div class="izquierda">
+        <a href="#">Política de cookies</a>
+        <a href="#">Política de privacidad</a>
+        <a href="#">Condiciones generales</a>
+      </div>
+      
+      <div class="cent">
+        <a href="#">Aviso legal</a>
+        <a href="#">Trabaja con nosotros</a>
+        <a href="#">¿Dónde estamos?</a>
+      </div>
+      <div class="loN">
+      <img id="logon" alt="logotiponeg" src="@/assets/logoN.png" />
+        </div>
+
+     
+<div class="redes">
+  <div>
+  <h3> ¡Síguenos!</h3>
+  </div>
+  <div class="red">
+       <a href="#" id="face"><img id="f" alt="facebook" src="@/assets/face.png" /></a>
+        <a href="#"  id="twitter"><img id="t" alt="twitter" src="@/assets/twitter.png" /></a>
+         <a href="#"  id="instagram"><img id="i" alt="instagram" src="@/assets/insta.png" /></a>
+
+         </div>
+    </div>
+
+    <div class="pagos">
+  <div>
+  <h3> Formas de pago</h3>
+  </div>
+  <div class="formap">
+       <a href="#" id="credit"><img id="c" alt="facebook" src="@/assets/credit.png" /></a>
+        <a href="#"  id="bizum"><img id="b" alt="twitter" src="@/assets/bizum.png" /></a>
+         <a href="#"  id="pay"><img id="p" alt="instagram" src="@/assets/paypal.png" /></a>
+
+         </div>
+    </div>
+    
+   
+      </div> 
+       <div class="copy">
+      <p>&#169; 2022 Irina Casas Duarte</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.red{
+ display:flex;
+ gap:1rem;
+
+ 
+}
+.redes{
+text-align: center;
+  margin-left:18rem;
+}
+.pagos{
+
+text-align: center;
+   margin-left:15rem;
+}
+
+.formap{
+display:flex;
+ gap:1rem;
+}
+
+ .redes h3{
+
+    font-family: "Urbanist";
+  color: rgb(223, 223, 223);
+  
+}
+
+.pagos h3{
+ font-family: "Urbanist";
+  color: rgb(223, 223, 223);
+  margin-left:-1rem;
+
+}
+
+#f{
+  width: 2rem;
+}
+#t{
+  width: 2rem;
+}
+#i{
+  width: 2rem;
+}
+#c{
+  width: 2.5rem;
+}
+#b{
+  width: 2.5rem;
+}
+#p{
+  width: 2.5rem;
+}
+#logon{
+  width: 8rem;
+  padding-top:2rem;
+}
+.pie {
+  display: flex;
+  flex-direction: column;
+  margin-top:2rem;
+}
+.pie p {
+  font-family: "Urbanist";
+  color: #333333(223, 223, 223);
+  text-align: center;
+}
+.enlaces {
+  display: flex;
+  
+
+  border-top-left-radius: 10px 10px;
+  border-top-right-radius: 10px 10px;
+  align-items: center;
+ 
+  
+  padding-bottom: 2rem;
+
+  background-color: #333333;
+}
+
+.enlaces a {
+  text-decoration: none;
+  font-size: 1.2em;
+  font-weight: bolder;
+
+  font-family: "Urbanist";
+  color: rgb(223, 223, 223);
+}
+
+.izquierda {
+  margin-left: 2rem;
+  display: flex;
+  flex-direction: column;
+  padding-right: 10rem;
+
+}
+
+.cent {
+  display: flex;
+  flex-direction: column;
+  margin-right: 2rem;
+  padding-right: 22rem;
+}
+</style>
